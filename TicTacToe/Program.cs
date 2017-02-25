ca55592b0a2a63c39ed48ea7f506bca82dcc8088
Matthew <matthew.christeyreid@gmail.com>
2017-02-25 18:51:04 +0000

    Basic tic tac toe console game

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,8 +6,84 @@ using System.Threading.Tasks;
 
 namespace TicTacToe {
     class Program {
+        static int _player = 1;
+        static bool isrunning = true;
+        static string[] _array = new string[] { "-", "-", "-",
+                                                "-", "-", "-",
+                                                "-", "-", "-" };
+
         static void Main(string[] args) {
-            string[] gamearray = new string[] { "", "" };
+            while (true) {
+                drawBoard();
+
+                p1move();
+                drawBoard();
+                if (!isrunning)
+                    break;
+
+                p2move();
+                drawBoard();
+                if (!isrunning)
+                    break;
+            }
+            
+            Console.WriteLine("GAME OVER, PLAYER {0} WINS", _player);
+            Console.ReadKey();
+        }
+
+        static void p1move() {
+            _player = 1;
+            bool istakingturn = true;
+            do {
+                char _input = Console.ReadKey(true).KeyChar;
+                if (_array[int.Parse(_input.ToString()) - 1] == "-") {
+                    _array[int.Parse(_input.ToString()) - 1] = "O";
+                    istakingturn = false;
+                }
+            } while (istakingturn);
+
+            checkWinner();
+        }
+
+        static void p2move() {
+            _player = 2;
+            bool istakingturn = true;
+            do {
+                char _input = Console.ReadKey(true).KeyChar;
+                if (_array[int.Parse(_input.ToString()) - 1] == "-") {
+                    _array[int.Parse(_input.ToString()) - 1] = "X";
+                    istakingturn = false;
+                }
+            } while (istakingturn);
+
+            checkWinner();
+        }
+
+        static void checkWinner() {
+            if (_array[0] == _array[1] && _array[0] == _array[2] && _array[0] != "-")
+                isrunning = false;
+            if (_array[3] == _array[4] && _array[3] == _array[5] && _array[3] != "-")
+                isrunning = false;
+            if (_array[6] == _array[7] && _array[6] == _array[8] && _array[6] != "-")
+                isrunning = false;
+            if (_array[0] == _array[3] && _array[0] == _array[6] && _array[0] != "-")
+                isrunning = false;
+            if (_array[1] == _array[4] && _array[1] == _array[7] && _array[1] != "-")
+                isrunning = false;
+            if (_array[2] == _array[5] && _array[2] == _array[8] && _array[2] != "-")
+                isrunning = false;
+            if (_array[0] == _array[4] && _array[0] == _array[8] && _array[0] != "-")
+                isrunning = false;
+            if (_array[2] == _array[4] && _array[2] == _array[6] && _array[2] != "-")
+                isrunning = false;
+        }
+
+        static void drawBoard() {
+            Console.Clear();
+            Console.WriteLine("It is player {0}'s go", _player);
+            Console.WriteLine(_array[0] + _array[1] + _array[2]);
+            Console.WriteLine(_array[3] + _array[4] + _array[5]);
+            Console.WriteLine(_array[6] + _array[7] + _array[8]);
         }
     }
 }
