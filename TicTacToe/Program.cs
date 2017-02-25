b5a7895a2c60fc00d7755547da48bee01befe71e
Matthew <matthew.christeyreid@gmail.com>
2017-02-25 17:06:52 +0000

    update

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace TicTacToe {
     class Program {
         static void Main(string[] args) {
+            string[] gamearray = new string[] { "", "" };
         }
     }
 }
